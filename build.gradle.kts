e3b8257b1e46f9419dcb2e651ba0bf0a6d7a78b0
남경호 <veluxer62@gmail.com>
2022-01-23 03:46:25 +0900

    fixup

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.mockito:mockito-inline:4.2.0")
 }
 
 tasks.withType<KotlinCompile> {
